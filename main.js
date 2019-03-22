9bf5275ad7751ff105dafd5788a88dfc194796d7
Ziyi <shangyuyu@sjtu.edu.cn>
2019-03-22 18:41:52 +0800

    [three-example buffergeometry] - original - 16w tri

--- main.js
+++ main.js
@@ -17,49 +17,129 @@ animate();
 
 function init() {
 
-    camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 2, 2000);
-    camera.position.z = 1000;
+    camera = new THREE.PerspectiveCamera(27, window.innerWidth / window.innerHeight, 1, 3500);
+    camera.position.z = 2750;
 
     //////////////////////////////////////////////////////////////
 
     scene = new THREE.Scene();
-    scene.fog = new THREE.FogExp2( 0x000000, 0.001 );
+    scene.background = new THREE.Color( 0x050505 );
+    scene.fog = new THREE.Fog( 0x050505, 2000, 3500 );
+
+    // Light
+    scene.add( new THREE.AmbientLight(0x444444) );
+
+    let light1 = new THREE.DirectionalLight( 0xffffff, 0.5 );
+    light1.position.set(1, 1, 1);
+    scene.add(light1);
+
+    let light2 = new THREE.DirectionalLight( 0xffffff, 1.5 );
+    light2.position.set( 0, -1, 0 );
+    scene.add(light2);
 
     // Geometry
+    let triangles = 160000;
+
     let geometry = new THREE.BufferGeometry();
 
-    let sprite = new THREE.TextureLoader().load( "./disc.png" );
+    let positions = [];
+    let normals = [];
+    let colors = [];
 
-    let vertice = [];
-    for (let i = 0; i < 10000; i++) {
+    let color = new THREE.Color();
 
-        let x = 2000 * Math.random() - 1000;
-        let y = 2000 * Math.random() - 1000;
-        let z = 2000 * Math.random() - 1000;
+    let n = 800, n2 = n / 2;
+    let d = 12, d2 = d / 2;
 
-        vertice.push(x, y, z);
+    let va = new THREE.Vector3();
+    let vb = new THREE.Vector3();
+    let vc = new THREE.Vector3();
+
+    let cb = new THREE.Vector3();
+    let ab = new THREE.Vector3();
+
+    for (let i = 0; i < triangles; i++) {
+
+        // position
+        let x = Math.random() * n - n2;
+        let y = Math.random() * n - n2;
+        let z = Math.random() * n - n2;
+        let ax = x + Math.random() * d - d2;
+        let ay = y + Math.random() * d - d2;
+        let az = z + Math.random() * d - d2;
+        let bx = x + Math.random() * d - d2;
+        let by = y + Math.random() * d - d2;
+        let bz = z + Math.random() * d - d2;
+        let cx = x + Math.random() * d - d2;
+        let cy = y + Math.random() * d - d2;
+        let cz = z + Math.random() * d - d2;
+
+        positions.push(ax, ay, az);
+        positions.push(bx, by, bz);
+        positions.push(cx, cy, cz);
+
+        // face normals
+        va.set(ax, ay, az);
+        vb.set(bx, by, bz);
+        vc.set(cx, cy, cz);
+        cb.subVectors(vc, vb);
+        ab.subVectors(va, vb);
+        cb.cross(ab);
+
+        cb.normalize();
+
+        let nx = cb.x;
+        let ny = cb.y;
+        let nz = cb.z;
+
+        normals.push(nx, ny, nz);
+        normals.push(nx, ny, nz);
+        normals.push(nx, ny, nz);
+
+        // Color
+        let vx = (x/n) + 0.5;
+        let vy = (y/n) + 0.5;
+        let vz = (z/n) + 0.5;
+
+        color.setRGB(vx, vy, vz);
+
+        colors.push(color.r, color.g, color.b);
+        colors.push(color.r, color.g, color.b);
+        colors.push(color.r, color.g, color.b);
     }
-    geometry.addAttribute("position", new THREE.Float32BufferAttribute(vertice, 3));
 
-    // Material
-    material = new THREE.PointsMaterial( { 
-        size: 35, 
-        sizeAttenuatation: false, 
-        map: sprite, 
-        alphaTest: 0.5, 
-        transparent: true, 
+    geometry.addAttribute("position", new THREE.Float32BufferAttribute(positions, 3));
+    geometry.addAttribute("normal", new THREE.Float32BufferAttribute(normals, 3));
+    geometry.addAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
+
+    positions = undefined;
+    normals = undefined;
+    colors = undefined;
+
+    geometry.computeBoundingSphere();
+
+    let material = new THREE.MeshPhongMaterial( {
+        color: 0xaaaaaa, 
+        specular: 0xffffff, 
+        shininess: 250, 
+        side: THREE.DoubleSide, 
+        vertexColors: THREE.VertexColors
     } );
-    material.color.setHSL( 1.0, 0.3, 0.7);
 
+    
     // bind Geometry and Material
-    let particles = new THREE.Points(geometry, material);
-    scene.add(particles);
+    let mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
     //////////////////////////////////////////////////////////////
 
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
+
+    renderer.gammaInput = true;
+    renderer.gammaOutput = true;
+
     document.body.appendChild( renderer.domElement );
 
     //////////////////////////////////////////////////////////////
@@ -69,15 +149,7 @@ function init() {
 
     //////////////////////////////////////////////////////////////
 
-    let gui = new dat.GUI();
 
-    gui.add( material, "sizeAttenuation" ).onChange( function () {
-
-        material.needsUpdate = true;
-
-    } );
-
-    gui.open();
 
     //////////////////////////////////////////////////////////////
 
@@ -131,15 +203,10 @@ function animate() {
 
 function render() {
 
-    let time = Date.now() * 0.00005;
-
-    camera.position.x += (mouseX - camera.position.x) * 0.05;
-    camera.position.y += ( - mouseY - camera.position.y) * 0.05;
+    camera.position.x += (mouseX - camera.position.x) * 0.5;
+    camera.position.y += ( - mouseY - camera.position.y) * 0.5;
 
     camera.lookAt( scene.position );
-
-    let h = (360 * (1.0 + time) % 360) / 360;
-    material.color.setHSL(h, 0.5, 0.5);
 
     renderer.render( scene, camera );
 }
