eeb1153e2f08a8ee5ff864df8a21e4c0bfded71b
Ziyi <shangyuyu@sjtu.edu.cn>
2019-04-08 17:48:35 +0800

    backup2

--- js/data.js
+++ js/data.js
@@ -17,9 +17,55 @@ class DataManager {
 
 Object.assign(DataManager.prototype, {
 
-    loadData: function () {
-        
+    loadData: function (file) {
+
+        let loader = new THREE.FileLoader();
+        loader.load(file, onLoad);
     },
+
+    onLoad: function (data_) {
+
+        let data = data_.split(/(\s+)/).filter( e => e.trim().length > 0 );
+        
+        const chr = Number(data[0]);
+        const resolution = Number(data[1]);
+        const startPos = Number(data[2]);
+        const dataScale = 50.0;
+        const objectSize  = 200;  // 200 points as an object
+
+        data.splice(0, 3);  // delete chr, resolution, startPos
+
+        // this.objects
+        let index = 0;
+        let count = 0;
+        let tempVec3Array = [];
+        for (let i = 0; i < data.length; i+=4) {
+            
+            count += 1;
+            if (data[i+1] === "nan") continue;
+
+            tempVec3Array.push( new THREE.Vector3( Number(data[i+1])*dataScale, Number(data[i+2])*dataScale, Number(data[i+3])*dataScale ) );
+
+            if (count == objectSize) {
+
+                count = 0;
+                this.objects[index].geometry.push( new THREE.CatmullRomVurve3(tempVec3Array) );
+                // locus? FIXME
+                tempVec3Array = [];
+                index += 1;
+            }
+        }
+        if (count > 0) {
+            this.objects[index].geometry.push( new THREE.CatmullRomVurve3(tempVec3Array) );
+            tempVec3Array = [];
+        }
+
+        // this.rawData
+        this.rawData[chr] = data;
+        data = [];
+
+        init();
+    }
 } );
 
 // End of data.js
