bd2db52db149b70446030e2df746569651151ce5
Ziyi <shangyuyu@sjtu.edu.cn>
2019-05-16 18:55:30 +0800

    Start to write documentation

--- public/javascripts/advanced.js
+++ public/javascripts/advanced.js
@@ -15,7 +15,7 @@ const advancedConfig = {
     mouseStayTime: 150,         // Mouse pick will be called after mouse stay still for 'mouseStayTime' ms
                                 // NOTE: Suggested that 'mouseStayTime' + 100 < 'mousePickInterval'
     autoTransparentDotProduct: 0.9,  // (0, 1) Conical frustum sin[(R1-R2) / h]  REF:http://mathworld.wolfram.com/ConicalFrustum.html
-    autoTransparentLength: 20,       // (0, 100) Conical frustum top radius R2
+    autoTransparentLength: 30,       // (0, 100) Conical frustum top radius R2
 };
 
 // End of advanced.js
